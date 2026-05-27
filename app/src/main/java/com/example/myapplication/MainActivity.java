bb1964cc2bd1e7c7c8aea1fa84f3d5c1f8e786de
cardosohttps <gustavo.c2007@aluno.ifsc.edu.br>
2026-05-27 15:06:29 -0300

    sorte

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    int contador=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.tv);
+
+
+        b.setOnClickListener(v -> {
+            contador++;
+            Random random= new Random();
+            int r = random.nextInt(100);
+            tv.setText(Integer.toString(r));
+
+        });
+
+    }
+}
